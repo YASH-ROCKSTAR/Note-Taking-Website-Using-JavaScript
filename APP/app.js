d5eeb2d343118a89fde897cdc832a1ff9244e713
YASH-ROCKSTAR <yash.nigam_cs19@gla.ac.in>
2022-01-30 18:03:29 +0530

    final changes done

--- APP/app.js
+++ APP/app.js
@@ -5,6 +5,7 @@ let addBtn=document.getElementById('addbtn');
 addBtn.addEventListener("click",function(e){
 
     let addTxt= document.getElementById("addtxt");
+    let addTitle=document.getElementById("addTitle")
     let notes=localStorage.getItem("notes");
     if(notes==null){
         notesobj=[];
@@ -12,7 +13,11 @@ addBtn.addEventListener("click",function(e){
     else{
         notesobj=JSON.parse(notes);
     }
-    notesobj.push(addTxt.value);
+    let myobj={
+        title:addTitle.value,
+        text:addTxt.value
+    }
+    notesobj.push(myobj);
     localStorage.setItem("notes", JSON.stringify(notesobj));
     addTxt.value= "";
     // console.log(notesobj);
@@ -31,8 +36,8 @@ function showNotes(){
         html += ` 
             <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Note ${index+1}</h5>
-                    <p class="card-text">${element}</p>
+                    <h5 class="card-title">${element.title}</h5>
+                    <p class="card-text">${element.text}</p>
                     <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Node</button>
                 </div>
             </div>
